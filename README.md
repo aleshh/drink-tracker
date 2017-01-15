6b9b90bbea198be9b1203cfa3d23a3d868dc11e7
Alesh Houdek <mail@alesh.com>
2017-01-15 17:17:44 -0500

    three-panel interface working

---
+++ README.md
@@ -0,0 +1,5 @@
+# Drink Tracker
+
+Right now this is mostly an interface playground.
+
+Uses [slick](https://github.com/kenwheeler/slick/) to create a three-panel swipable interface ala Snapchat.
---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function() {
+
+  $('.panel-container').slick({
+    // http://kenwheeler.github.io/slick/
+    infinite: false,
+    arrows: false,
+    edgeFriction: 0,
+    initialSlide: 1,
+    speed: 300,
+    touchThreshold: 3
+  });
+
+});
